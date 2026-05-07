1a4d30079be9128ab946c62e57ff716fa53812ae
HectorFacundoArana <hfacundoarana@gmail.com>
2026-05-06 22:15:14 -0300

    First Commit

---
+++ TADCentroImpresion.py
@@ -0,0 +1,30 @@
+from TADTrabajo import *
+from TADCola import *
+
+def crearCentro():
+	#Crea un centro de impresion vacio
+	return []
+
+def agregarTrabajo(centro,trabajo):
+	#Agrega un trabajo al centro de impresion
+	centro.append(trabajo)
+
+def eliminarTrabajo(centro,trabajo):
+	#Elimina un trabajo especifico
+	centro.remove(trabajo)
+
+def recuperarTrabajo(centro,i):
+	#Retorna el trabajo de la posicion iesima
+	return centro[i]
+
+def tamanio(centro):
+	#Retorna cantidad de trabajos
+	return len(centro)
+
+def existeTrabajo(centro,trabajo):
+	#Comprueba si existe una venta
+	return trabajo in centro
+
+def centroVacio(centro):
+	#Retorna True si centro no tiene elementos
+	return esVacia(centro)
